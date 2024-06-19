3f5f574f2bce556156b96873bbba45a16fae5e98
D20125529 Anil Kunchala <d20125529@ADAPT-MASTER.campus.tudublin.ie>
2024-06-19 05:52:18 +0100

    added wf_vs_org configs

---
+++ configs/tubelet_dataset/timesformer/timesformer_tubelet_dataset_pretrained.py
@@ -0,0 +1,110 @@
+_base_ = '../../recognition/timesformer/timesformer_spaceOnly_8xb8-8x32x1-15e_kinetics400-rgb.py'
+
+root_dir = "/home/ICTDOMAIN/d20125529/act_tubelet_dataset_gen/TUBELET_DATASET_FINAL_ORG"
+
+# dataset settings
+dataset_type = 'RawframeDataset'
+data_root = root_dir
+data_root_val = root_dir
+data_root_test = root_dir
+split = 1  # official train/test splits. valid numbers: 1, 2, 3
+ann_file_train = F"{root_dir}/tubelet_train.txt"
+ann_file_val = F"{root_dir}/tubelet_test.txt"
+ann_file_test = F"{root_dir}/tubelet_test.txt"
+
+model = dict(
+            backbone=dict(attention_type='divided_space_time',
+            pretrained=None),
+            cls_head=dict(
+                num_classes = 7            
+                ))
+
+
+file_client_args = dict(io_backend='disk')
+
+train_pipeline = [
+    # dict(type='DecordInit', **file_client_args),
+    dict(type='SampleFrames', clip_len=8, frame_interval=32, num_clips=1),
+    # dict(type='DecordDecode'),
+    dict(type='RawFrameDecode', **file_client_args),
+    # dict(type='RandomRescale', scale_range=(256, 320)),
+    # dict(type='RandomCrop', size=224),
+    dict(type='Resize', scale=(224, 224), keep_ratio=False),
+    dict(type='Flip', flip_ratio=0.5),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='PackActionInputs')
+]
+val_pipeline = [
+    # dict(type='DecordInit', **file_client_args),
+    dict(
+        type='SampleFrames',
+        clip_len=8,
+        frame_interval=32,
+        num_clips=1,
+        test_mode=True),
+    # dict(type='DecordDecode'),
+    dict(type='RawFrameDecode', **file_client_args),
+    # dict(type='Resize', scale=(-1, 256)),
+    # dict(type='CenterCrop', crop_size=224),
+    dict(type='Resize', scale=(224, 224), keep_ratio=False),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='PackActionInputs')
+]
+test_pipeline = [
+    # dict(type='DecordInit', **file_client_args),
+    dict(
+        type='SampleFrames',
+        clip_len=8,
+        frame_interval=32,
+        num_clips=1,
+        test_mode=True),
+    # dict(type='DecordDecode'),
+    dict(type='RawFrameDecode', **file_client_args),
+    # dict(type='Resize', scale=(-1, 224)),
+    # dict(type='ThreeCrop', crop_size=224),
+    dict(type='Resize', scale=(224, 224), keep_ratio=False),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='PackActionInputs')
+]
+train_dataloader = dict(
+    batch_size=4,
+    num_workers=8,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    dataset=dict(
+        type=dataset_type,
+        ann_file=ann_file_train,
+        data_prefix=dict(img=data_root),
+        pipeline=train_pipeline))
+val_dataloader = dict(
+    batch_size=4,
+    num_workers=8,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        ann_file=ann_file_val,
+        data_prefix=dict(img=data_root_val),
+        pipeline=val_pipeline,
+        test_mode=True))
+test_dataloader = dict(
+    batch_size=1,
+    num_workers=8,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        ann_file=ann_file_test,
+        data_prefix=dict(img=data_root_val),
+        pipeline=test_pipeline,
+        test_mode=True))
+
+val_evaluator = dict(type='AccMetric')
+test_evaluator = val_evaluator
+
+train_cfg = dict(
+    type='EpochBasedTrainLoop', max_epochs=35, val_begin=1, val_interval=1)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+
+load_from='https://download.openmmlab.com/mmaction/v1.0/recognition/timesformer/timesformer_divST_8xb8-8x32x1-15e_kinetics400-rgb/timesformer_divST_8xb8-8x32x1-15e_kinetics400-rgb_20220815-a4d0d01f.pth'
